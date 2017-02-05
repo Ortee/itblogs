065a303e2c2596b34445e29edb539e4ade6dc25a
Mateusz <kontakt@mateuszoracz.pl>
2017-02-05 23:39:28 +0100

    catch API errors

--- server/server.js
+++ server/server.js
@@ -12,29 +12,33 @@ app.use(function(req, res, next) {
 });
 
 app.get('/api/posts', function (req, res) {
-  request({
-    uri: req.query.link,
-    method: "GET",
-    timeout: 10000,
-    followRedirect: true,
-    maxRedirects: 10
-  }, function(error, response, body) {
-    parseString(body, function (err, result) {
-        console.log(req.query.link);
-        var reqbody = result.rss.channel[0];
-        var posts = [];
-        reqbody.item.map( post => {
-          posts.push({
-            author: reqbody.title,
-            title: post.title,
-            link: post.link,
-            date: new Date(post.pubDate).getTime(),
-            description: replace(post.description.toString()).substr(0, 500),
-          })
-        });
-        res.send(posts).status(200);
+  try{
+    request({
+      uri: req.query.link,
+      method: "GET",
+      timeout: 10000,
+      followRedirect: true,
+      maxRedirects: 10
+    }, function(error, response, body) {
+      parseString(body, function (err, result) {
+          console.log(req.query.link);
+          var reqbody = result.rss.channel[0];
+          var posts = [];
+          reqbody.item.map( post => {
+            posts.push({
+              author: reqbody.title,
+              title: post.title,
+              link: post.link,
+              date: new Date(post.pubDate).getTime(),
+              description: replace(post.description.toString()).substr(0, 500),
+            })
+          });
+          res.send(posts).status(200);
+      });
     });
-  });
+  } catch (err) {
+    console.log(err);
+  }
 })
 
 app.listen(3000);
